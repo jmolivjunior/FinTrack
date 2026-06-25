0b74f755f015ab6a7a5c590bafe90c92d89b3534
Jose Maria <jm.olivjunior@gmail.com>
2026-06-25 21:02:13 +0100

    Initial commit - FinTrack API base structure

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using FinTrack.API.Models;
+
+namespace FinTrack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private static List<Transaction> transactions = new List<Transaction>();
+
+        [HttpGet]
+        public ActionResult<List<Transaction>> GetAll()
+        {
+            return Ok(transactions);
+        }
+
+        [HttpPost]
+        public ActionResult<Transaction> Create(Transaction transaction)
+        {
+            transaction.Id = transactions.Count + 1;
+            transaction.Date = DateTime.Now;
+            transactions.Add(transaction);
+            return Ok(transaction);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var transaction = transactions.FirstOrDefault(t => t.Id == id);
+            if (transaction == null) 
+            return NotFound();
+            transactions.Remove(transaction);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace FinTrack.API.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;        public Decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Category { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+    }
+}
