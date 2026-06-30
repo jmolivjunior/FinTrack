6366e57c7dc517bccc615c2f67308ab6acefac90
Jose Maria <jm.olivjunior@gmail.com>
2026-06-30 21:22:33 +0100

    Fix - Auto migrate database on startup

--- Program.cs
+++ Program.cs
@@ -76,6 +76,12 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseCors("AllowReact");
